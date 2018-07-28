8005887e3c3a3df1c41b8f214968d7ef1de4394c
Sebastian Krysmanski <noreply@manski.net>
2018-07-28 15:19:57 +0200

    Certs are now stored in a different directory

--- request-cert-for-vault.sh
+++ request-cert-for-vault.sh
@@ -5,5 +5,6 @@ DOMAIN_NAME=$(hostname)
 
 CERT_DATA=$(./vault write -format=json pki/issue/$ROLE_NAME common_name=$DOMAIN_NAME)
 
-echo $CERT_DATA | jq -r .data.certificate,.data.issuing_ca > config/vault.crt
-echo $CERT_DATA | jq -r .data.private_key > config/vault.key
+mkdir -p certs
+echo $CERT_DATA | jq -r .data.certificate,.data.issuing_ca > certs/vault.crt
+echo $CERT_DATA | jq -r .data.private_key > certs/vault.key
